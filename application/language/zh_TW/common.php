d346e2dc91efff605e5da10e00794e7f457b1e2b
蒼時弦也 <elct9620@frost.tw>
2012-07-15 16:28:12 +0800

    正體中文語言包內容增加

--- application/language/zh_TW/common.php
+++ application/language/zh_TW/common.php
@@ -3,11 +3,15 @@
 return array(
   "subject" => "主旨",
   "content" => "內容",
-  "comment" => "留言"
+  "comment" => "留言",
+  "no_comments" => "沒有任何留言",
   "profile"  => "個人檔案",
   "login" => "登入",
   "logout" => "登出",
   "register" => "註冊",
   "nickname" => "暱稱",
-  "save" => "儲存"
+  "save" => "儲存",
+  "username" => "帳號",
+  "password" => "密碼",
+  "password_confirm" => "確認密碼"
 );
