6c7c1b6785d59bc2b2f26e5156e53ff7ac92fc9a
蒼時弦也 <elct9620@frost.tw>
2012-07-15 09:57:25 +0800

    新增會員資料表

---
+++ application/migrations/2012_07_15_015438_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+class Create_Users_Table {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    Schema::create('users', function($table){
+      $table->increments('ID');
+      $table->string('username');
+      $table->string('password');
+      $table->string('nickname');
+
+      $table->unique('username');
+    });
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    Schema::drop('users');
+	}
+
+}
