2debd6c7862a62d5ed27f2a9ea945c7a9e041cc1
蒼時弦也 <elct9620@frost.tw>
2012-07-15 11:43:18 +0800

    增加註冊頁面

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -6,17 +6,36 @@
 
     public function get_new()
     {
-
+      return View::make('user.new');
     }
 
     public function post_new()
     {
+      $inputs = Input::all();
+      $rules = array(
+        'username' => 'required|unique:users|alpha_num',
+        'password' => 'required|min:6',
+        'password_confirm' => 'required|same:password'
+      );
+
+      $validation = Validator::make($inputs, $rules);
+
+      if( $validation->fails() ) {
+        return Redirect::to('user/new')->with_errors($validation);
+      }
+
+      User::create(array(
+        'username' => $inputs['username'],
+        'password' => Hash::make($inputs['password'])
+      ));
+
+      return Redirect::home();
 
     }
 
     public function get_profile()
     {
-
+      return View::make('user.profile');
     }
 
     public function post_profile()
@@ -26,17 +45,28 @@
 
     public function get_login()
     {
-
+      return View::make('user.login');
     }
 
     public function post_login()
     {
+      $inputs = Input::all();
+      $credentials = array(
+        'username' => $inputs['username'],
+        'password' => $inputs['password']
+      );
 
+      if( !Auth::attempt($credentials) ){
+        return Redirect::home();
+      }
+
+      return Redirect::to('session/login');
     }
 
-    public get_logout()
+    public function get_logout()
     {
-
+      Auth::logout();
+      return Redirect::home();
     }
 
   }
--- application/views/user/new.blade.php
+++ application/views/user/new.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Simple Guestbook</title>
+	<title>Register - Simple Guestbook</title>
 	<meta name="viewport" content="width=device-width">
 	{{ HTML::style('laravel/css/style.css') }}
   <style>
@@ -20,43 +20,34 @@
 		<header>
 			<h1>Guestbook</h1>
 			<h2>A simple guestbook</h2>
-      @if ( Auth::check() )
-        Hello, {{ Auth::user()->nickname || Auth::user()->username }} | {{ HTML::link('user/logout', 'Logout') }}
-      @else
-        {{ HTML::link('user/new', 'Register') }} | {{ HTML::link('user/login', 'Login') }}
-      @endif
+
 			<p class="intro-text" style="margin-top: 45px;">
 			</p>
 		</header>
 		<div role="main" class="main">
-      <div class="new">
+      <div class="home">
         <?php $errorMessages = $errors->all('<pre>:message</pre>'); ?>
         @foreach ($errorMessages as $message)
           {{ $message }}
         @endforeach
         {{ Form::open() }}
         <p>
-          {{ Form::label('subject', 'Subject') }}
-          {{ Form::text('subject') }}
+          {{ Form::label('username', 'Username') }}
+          {{ Form::text('username') }}
         </p>
         <p>
-          {{ Form::label('content', 'Content') }}
-          {{ Form::textarea('content') }}
+          {{ Form::label('password', 'Password') }}
+          {{ Form::password('password') }}
         </p>
         <p>
-          {{ Form::submit('Comment') }}
+          {{ Form::label('password_confirm', 'Password Confirm') }}
+          {{ Form::password('password_confirm') }}
+        </p>
+        <p>
+          {{ Form::submit('Regiester') }}
         </p>
         {{ Form::close() }}
       </div>
-      <hr />
-			<div class="home">
-        @forelse ($comments->results as $comment)
-          <h2>{{ e($comment->subject) }}</h2>
-          <pre>{{ e($comment->content) }}</pre>
-        @empty
-          <pre> No comment be found! </pre>
-        @endforelse
-			</div>
 		</div>
 	</div>
 </body>
