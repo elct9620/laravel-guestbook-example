659f42b05d8de38f14bd9c0133b88023966ddcbe
蒼時弦也 <elct9620@frost.tw>
2012-07-15 16:09:19 +0800

    加入語言包

---
+++ application/language/zh_TW/common.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+  "subject" => "主旨",
+  "content" => "內容",
+  "comment" => "留言"
+  "profile"  => "個人檔案",
+  "login" => "登入",
+  "logout" => "登出",
+  "register" => "註冊",
+  "nickname" => "暱稱",
+  "save" => "儲存"
+);
